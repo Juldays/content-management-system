0037619b19cd1c839bd36778c21ae8fdc03ff1d0
Julianna Diaz <jdjuliannadiaz@gmail.com>
2018-07-05 10:43:20 -0700

    Add files via upload

---
+++ ICMSService.cs
@@ -0,0 +1,24 @@
+﻿using Models.Domain;
+using Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ICMSService
+    {
+        List<CMSTemplate> GetAll();
+        CMSTemplate GetById(int id);
+        int Create(CMSTemplateCreateRequest req);
+        void Update(CMSTemplateUpdateRequest req);
+        void Delete(int id);
+        List<CMSPage> GetAllPages();
+        CMSPage GetPageById(int id);
+        int CreatePage(CMSPageCreateRequest req);
+        void UpdatePage(CMSPageUpdateRequest req);
+        void DeletePage(int id);
+    }
+}
